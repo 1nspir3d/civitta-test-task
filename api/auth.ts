fe5301b35ded6b1a07493ac1d76a457874baadfd
gitlab runner <runner@axonesys.com>
2025-05-13 01:07:23 +0300

    feat: add validation to api methods

--- api/auth.ts
+++ api/auth.ts
@@ -1,48 +1,36 @@
-import api from "./axiosInstance"
+import { accountSchema } from "../utils/schemas";
+import api from "./axiosInstance";
 
 type TSignupPayload = {
-  name: string,
-  email: string,
-  password: string,
-}
+  name: string;
+  email: string;
+  password: string;
+};
 
 interface ISignupResponse {
-  message: string
-  nextStep: string
+  message: string;
+  nextStep: string;
   basicAuthCredentials: {
-    username: string
-    password: string
-  }
+    username: string;
+    password: string;
+  };
 }
 
 type TLoginAuth = {
-  username: string,
-  password: string,
-} 
-
-export interface ITransaction {
-    name: string
-    bank: string
-    time: string
-    amount: number
-  }
-
-export interface IAccountData {
-  accountType: string
-  availableBalance: number
-  currency: string
-  dateAdded: string
-  transactions: ITransaction[]
-}
+  username: string;
+  password: string;
+};
 
 export const signup = async (payload: TSignupPayload) => {
-  const res = await api.post('/signup', payload)
-  return res.data as ISignupResponse
-}
+  const res = await api.post("/signup", payload);
+  return res.data as ISignupResponse;
+};
 
 export const fetchAccount = async (auth: TLoginAuth) => {
-  const res = await api.get('/account', {
-    auth
-  })
-  return res.data as IAccountData
-}
+  const res = await api.get("/account", {
+    auth,
+  });
+
+  const parsedRes = accountSchema.validate(res.data);
+  return parsedRes;
+};
