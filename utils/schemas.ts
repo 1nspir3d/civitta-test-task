0a780be85a072e32cb4d972c0ef71f70375c067c
gitlab runner <runner@axonesys.com>
2025-05-13 01:05:34 +0300

    feat: add schemas to validate api response

--- utils/schemas.ts
+++ utils/schemas.ts
@@ -7,3 +7,21 @@ export const SignUpSchema = yup
   })
   .required()
 export type TSignUpFormData = yup.InferType<typeof SignUpSchema>
+
+export const transactionSchema = yup.object({
+  name: yup.string().required(),
+  bank: yup.string().required(),
+  time: yup.string().required(),
+  amount: yup.number().required(),
+});
+
+export const accountSchema = yup.object({
+  accountType: yup.string().required(),
+  availableBalance: yup.number().required(),
+  currency: yup.string().required(),
+  dateAdded: yup.string().required(),
+  transactions: yup.array().of(transactionSchema).required(),
+});
+
+export type TTransaction = yup.InferType<typeof transactionSchema>
+export type TAccountData = yup.InferType<typeof accountSchema>
